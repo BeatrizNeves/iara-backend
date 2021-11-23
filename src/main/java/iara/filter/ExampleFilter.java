af4cf9c4bbeb08efd51b7396576072a65d397cce
Beatriz Neves <beatriznevesporto@hotmail.com>
2021-11-23 18:13:03 -0300

    Criando estrutura de filtros para SQL

--- src/main/java/iara/filter/ExampleFilter.java
+++ src/main/java/iara/filter/ExampleFilter.java
@@ -1,5 +1,34 @@
 package iara.filter;
 
-public class ExampleFilter {
+import javax.persistence.criteria.Predicate;
 
+import org.springframework.data.jpa.domain.Specification;
+
+import iara.model.ExampleEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Builder
+public class ExampleFilter extends RepositoryFilter<ExampleEntity> {
+	
+	private String name;
+	
+	@Override
+    public Specification<ExampleEntity> get() {
+        return (root, query, cb) -> {
+            
+            query.orderBy(cb.asc(root.get("id")));
+            
+            Predicate genericTextFilter = like(cb, root.get("name"), name);
+            
+            return cb.and(genericTextFilter);
+        };
+    }
 }
--- src/main/java/iara/repository/ExampleRepository.java
+++ src/main/java/iara/repository/ExampleRepository.java
@@ -1,9 +1,14 @@
 package iara.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import iara.model.ExampleEntity;
 
 public interface ExampleRepository extends JpaRepository<ExampleEntity, Long> {
+	
+	List<ExampleEntity> findAll(Specification<ExampleEntity> filter);
 
 }
