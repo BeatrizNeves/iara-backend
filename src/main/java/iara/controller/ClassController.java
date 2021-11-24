29097f1379b89cc058766b60aae95d6ad473800b
Beatriz Neves <beatriznevesporto@hotmail.com>
2021-11-24 15:35:18 -0300

    Criando filtro para questões

--- src/main/java/iara/controller/ClassController.java
+++ src/main/java/iara/controller/ClassController.java
@@ -7,17 +7,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import iara.filter.QuestionFilter;
 import iara.service.CourseService;
+import iara.service.QuestionService;
 
 @RestController
-@RequestMapping("/course")
+@RequestMapping("/class")
 public class ClassController {
 	
 	@Autowired
-	private CourseService service;
+	private CourseService couseService;
+	
+	@Autowired
+	private QuestionService questionService;
 	
 	@GetMapping(value = {"", "/{classId}"})
 	public ResponseEntity<?> getById(@PathVariable(required = true) Long classId) {
-		return ResponseEntity.ok(service.findById(classId).get());
+		return ResponseEntity.ok(couseService.findById(classId).get());
+	}
+	
+	@GetMapping("/getCompleteClass/{classId}")
+	public ResponseEntity<?> getQuestionsByClassId(@PathVariable(required = true) Long classId) {
+		QuestionFilter filter = QuestionFilter.builder().classId(classId).build();
+		
+		return ResponseEntity.ok(questionService.findAll(filter.get()));
 	}
 }
---
+++ src/main/java/iara/filter/QuestionFilter.java
@@ -0,0 +1,34 @@
+package iara.filter;
+
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import iara.model.QuestionEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Builder
+public class QuestionFilter extends RepositoryFilter<QuestionEntity> {
+	
+	private Long classId;
+	
+	@Override
+    public Specification<QuestionEntity> get() {
+        return (root, query, cb) -> {
+            
+            query.orderBy(cb.asc(root.get("id")));
+            
+            Predicate genericTextFilter = equal(cb, root.get("Class_id_class"), classId.toString());
+            
+            return cb.and(genericTextFilter);
+        };
+    }
+}
