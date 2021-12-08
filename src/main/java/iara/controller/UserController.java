ab5740cdde6bcc0a48f2bbdf2b7861962e883e05
guazco <guazco@usp.br>
2021-12-08 03:15:33 -0300

    :bug: fixing simple cors problem

--- src/main/java/iara/controller/UserController.java
+++ src/main/java/iara/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import iara.service.UserService;
 import iara.service.UserValidationService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/user")
 public class UserController {
 
--- src/main/java/iara/controller/UserValidationController.java
+++ src/main/java/iara/controller/UserValidationController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import iara.model.UserValidationRequest;
 import iara.service.UserValidationService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/login")
 public class UserValidationController {
 	
